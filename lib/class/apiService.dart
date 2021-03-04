7df5acf91141fff17c88153c045e573e7ab61b8a
chadpong <p.chadpong@gmail.com>
2021-03-04 19:19:27 +0700

    add Simple AP Service

---
+++ lib/class/apiService.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+import 'package:ofp_flutter/class/constants.dart' as Constants;
+import 'package:ofp_flutter/model/ofp.dart';
+
+class APIService {
+  Future<OFP> getOFP() async {
+    String url = Constants.ENDPOINT + "/ofp/flight/tg110/2020/01/01";
+    try {
+      final response = await http.get(url);
+      if (response.statusCode != 200)
+        throw HttpException('${response.statusCode}');
+
+      OFP _ofp = OFP.fromJSON(jsonDecode(response.body));
+      return _ofp;
+    } on Exception catch (e) {
+      print(e);
+      return null;
+    }
+  }
+}
